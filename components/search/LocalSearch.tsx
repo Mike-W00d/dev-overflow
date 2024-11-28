4a90a1df6c40787d585d31c6a05d0a1d0b0551b4
MichaelWood <mgwood22@sky.com>
2024-11-28 13:51:19 +0000

    Created Local Search component and added into the home page along with some dummy questions

---
+++ components/search/LocalSearch.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import Image from "next/image";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+import { formUrlQuery, removeKeysFromUrlQuery } from "@/lib/url";
+
+import { Input } from "../ui/input";
+
+interface Props {
+  route: string;
+  imgSrc: string;
+  placeholder: string;
+  otherClasses?: string;
+}
+
+const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query") || "";
+  const pathname = usePathname();
+
+  const [searchQuery, setSearchQuery] = useState(query);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      /// adding the query string to the URL
+      if (searchQuery) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "query",
+          value: searchQuery,
+        });
+        router.push(newUrl, { scroll: false });
+        // removing the query string from the URL
+      } else {
+        if (pathname === route) {
+          const newUrl = removeKeysFromUrlQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["query"],
+          });
+          return router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 300); /// time fire the search query in  milliseconds
+    // debouncing the search query  so that it doesn't fire on every key stroke
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchQuery, router, route, searchParams, pathname]);
+
+  return (
+    <div
+      className={`background-light800_darkgradient flex min-h-[56px] 
+    grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
+    >
+      <Image
+        src={imgSrc}
+        width={24}
+        height={24}
+        alt="Search"
+        className="cursor-pointer"
+      />
+      <Input
+        type="text"
+        placeholder={placeholder}
+        value={searchQuery}
+        onChange={(e) => {
+          setSearchQuery(e.target.value);
+        }}
+        className="paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none"
+      />
+    </div>
+  );
+};
+
+export default LocalSearch;
